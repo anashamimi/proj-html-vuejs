4a960733a37235e792fb3f923b94eb2e24c1cb00
anashamimi <anashamimi2@gmail.com>
2023-04-27 19:09:48 +0200

    add component sectionTitle for all titles in the main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,15 +1,19 @@
 <template>
-    <Carousel/>
+    <Carousel />
+    <SectionTitle :title="'Featured Products'" :subTitle="'Must have products from our top sellers'"/>
+    
 </template>
 
 <script>
 import Carousel from './Carousel.vue';
-    export default {
-        name: 'MainComponent',
-        components: {
-            Carousel
-        }
+import SectionTitle from './SectionTitle.vue';
+export default {
+    name: 'MainComponent',
+    components: {
+        Carousel,
+        SectionTitle
     }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+            <hr>
+            <h2 class="text-center fw-bolder fs-1">{{ title }}</h2>
+            <hr>
+        </div>
+        <p class="text-center">{{ subTitle }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SectionTitle',
+        props: ['title', 'subTitle'],
+    }
+</script>
+
+<style lang="scss" scoped>
+hr {
+    color: var(--grey);
+    width: calc(100% / 2 - 200px);
+}
+
+h2{
+    font-family: 'Montserrat', sans-serif;
+}
+</style>
